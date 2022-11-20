b978e1c1c29796d49d49cabe524cce4866d51787
MohammedHammouri03 <97105790+MohammedHammouri03@users.noreply.github.com>
2022-11-20 12:56:31 +0100

    CSS fixes for Learnview and Testview

--- src/views/Learnview.vue
+++ src/views/Learnview.vue
@@ -1,51 +1,71 @@
 <template>
-  <div class="container">
-    <input type="checkbox" id="card-1"/>
-    <label class="card-container" for="card-1">
-      <div class="card-flip">
-        <div class="front card">
-          <div class="card-block">
-            <h1 class="card-title text-center">{{ persons.firstName }}</h1>
-          </div>
-          <h1>Click on the card to turn around</h1>
-        </div>
-        <div class="back card">
-          <div class="card-block">
-            <h1 class="card-title">Back Card</h1>
-          </div>
-          <h1>Click on the card to turn around</h1>
+  <head>
+    <title>Vocabularies</title>
+  </head>
+  <body>
+  <div class="container-fluid">
+    <div class="row row-cols 1 row-cols-md4 g-4">
+      <div class="col" v-for="vocabular in vocabularies" :key="vocabular.id">
+        <div class="box">
+   <span><h1>{{ vocabularies.indexOf(vocabular) + 1 }}/{{ vocabularies.length }}</h1>
+   <div class="word">
+     <h2>{{ vocabular.word }}</h2>
+   <div class="click">
+     <button class="btn btn-primary" @click="showDefinition(vocabular)"><svg xmlns="http://www.w3.org/2000/svg"
+                                                                             width="16" height="16" fill="currentColor"
+                                                                             class="bi bi-eye-fill" viewBox="0 0 16 16">
+  <path d="M10.5 8a2.5 2.5 0 1 1-5 0 2.5 2.5 0 0 1 5 0z"/>
+  <path d="M0 8s3-5.5 8-5.5S16 8 16 8s-3 5.5-8 5.5S0 8 0 8zm8 3.5a3.5 3.5 0 1 0 0-7 3.5 3.5 0 0 0 0 7z"/>
+</svg></button>
+     </div>
+     <div class="definition" v-if="vocabular.showDefinition">
+       <h3>{{ vocabular.translation }}</h3>
+     </div>
+   </div>
+   </span>
         </div>
       </div>
-    </label>
+    </div>
   </div>
-  <button type="button" class="btn btn-primary left">
-    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-left"
+  <button class="btn btn-primary" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasRight"
+          aria-controls="offcanvasRight">
+    <svg xmlns="http://www.w3.org/2000/svg" width="26" height="26" fill="black" class="bi bi-bookmark-plus"
          viewBox="0 0 16 16">
-      <path fill-rule="evenodd"
-            d="M15 8a.5.5 0 0 0-.5-.5H2.707l3.147-3.146a.5.5 0 1 0-.708-.708l-4 4a.5.5 0 0 0 0 .708l4 4a.5.5 0 0 0 .708-.708L2.707 8.5H14.5A.5.5 0 0 0 15 8z"/>
+      <path
+        d="M2 2a2 2 0 0 1 2-2h8a2 2 0 0 1 2 2v13.5a.5.5 0 0 1-.777.416L8 13.101l-5.223 2.815A.5.5 0 0 1 2 15.5V2zm2-1a1 1 0 0 0-1 1v12.566l4.723-2.482a.5.5 0 0 1 .554 0L13 14.566V2a1 1 0 0 0-1-1H4z"/>
+      <path
+        d="M8 4a.5.5 0 0 1 .5.5V6H10a.5.5 0 0 1 0 1H8.5v1.5a.5.5 0 0 1-1 0V7H6a.5.5 0 0 1 0-1h1.5V4.5A.5.5 0 0 1 8 4z"/>
     </svg>
   </button>
-  <button type="button" class="btn btn-primary middle">
-    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-clockwise"
-         viewBox="0 0 16 16">
-      <path fill-rule="evenodd" d="M8 3a5 5 0 1 0 4.546 2.914.5.5 0 0 1 .908-.417A6 6 0 1 1 8 2v1z"/>
-      <path d="M8 4.466V.534a.25.25 0 0 1 .41-.192l2.36 1.966c.12.1.12.284 0 .384L8.41 4.658A.25.25 0 0 1 8 4.466z"/>
-    </svg>
-  </button>
-  <button type="button" class="btn btn-primary right">
-    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-right"
-         viewBox="0 0 16 16">
-      <path fill-rule="evenodd"
-            d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8z"/>
-    </svg>
-  </button>
+
+  <div class="offcanvas offcanvas-end" tabindex="-1" id="offcanvasRight" aria-labelledby="offcanvasRightLabel">
+    <div class="offcanvas-header">
+      <h5 id="offcanvasRightLabel">Create a new Vocabulary</h5>
+      <button type="button" class="btn-close text-reset" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+    </div>
+    <div class="offcanvas-body">
+      <form>
+        <div class="mb-3">
+          <label for="word" class="form-label">Word</label>
+          <input type="text" class="form-control" id="word" v-model="word">
+        </div>
+        <div class="mb-3">
+          <label for="translation" class="form-label">Translation</label>
+          <input type="text" class="form-control" id="translation" v-model="translation">
+        </div>
+        <button type="submit" class="btn btn-primary" @click="createVocabulary(vocabularies2)">Submit</button>
+        <button type="reset" class="btn btn-danger" @click="resetForm">Reset</button>
+      </form>
+    </div>
+  </div>
+  </body>
 </template>
 <script>
 export default {
-  name: 'Learnview',
+  name: 'Testview',
   data () {
     return {
-      persons: []
+      vocabularies: []
     }
   },
   mounted () {
@@ -53,82 +73,134 @@ export default {
       method: 'GET',
       redirect: 'follow'
     }
-
-    fetch('http://localhost:8080/api/persons', requestOptions)
+    fetch('http://localhost:8080/api/vocabularies', requestOptions)
       .then(response => response.json())
-      .then(result => result.forEach(person => {
-        this.persons.push(person)
+      .then(result => result.forEach(vocabular => {
+        this.vocabularies.push(vocabular)
       }))
       .catch(error => console.log('error', error))
+  },
+  methods: {
+    showDefinition (vocabular) {
+      vocabular.showDefinition = !vocabular.showDefinition
+    },
+    data () {
+      return {
+        vocabularies2: [],
+        word: '',
+        translation: ''
+      }
+    },
+    methods: {
+      createVocabulary () {
+        const headers = new Headers()
+        headers.append('Content-Type', 'application/json')
+
+        const raw = JSON.stringify({
+          word: this.word,
+          translation: this.translation
+        })
+        const requestOptions = {
+          method: 'POST',
+          headers: headers,
+          body: raw,
+          redirect: 'follow'
+        }
+        fetch('http://localhost:8080/api/vocabularies', requestOptions)
+          .catch(error => console.log('error', error))
+      }
+    }
   }
 }
 </script>
 <style scoped>
 .btn-primary {
-  background-color: #1e90ff;
-  border-color: #1e90ff;
+  margin-left: auto;
 }
 
-.card-title {
-  font-size: 2em;
-  font-weight: 800;
-  line-height: 3em;
-  margin: 0;
+.box:hover {
+  transform: scale(1.05);
+}
+
+.word {
+  font-size: 1.5em;
+  font-weight: bold;
+  text-position: center;
+  margin: 8%;
+  color: #1c1d1e;
+}
+
+body {
+  background: #1c1d1e;
+  marign: 0;
   padding: 0;
-  text-align: center;
-  color: #141515;
+  display: grid;
+  place-items: center;
+  min-height: 100vh;
 }
 
-.card {
-  margin: 10px 10px;
-  border: 1px solid #ccc;
-  border-radius: 5px;
-  box-shadow: 0 1px 2px rgba(0, 0, 0, 0.2);
-  background-color: #f6f1f1;
-  transition: all 0.5s ease;
-  transform-style: preserve-3d;
+.box {
+  width: 300px;
+  height: 300px;
+  background-color: #d3cfcf;
+  border-radius: 10px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  transition: 0.5s;
+}
+
+.box {
   position: relative;
-  font-max-size: large;
-}
-
-.card-block {
-  padding: 10.5rem;
-}
-
-input[type='checkbox'] {
-  display: none;
-}
-
-.card-container {
+  width: 300px;
+  height: 200px;
+  border-radius: 20px;
   display: grid;
-  perspective: 700px;
-  margin-left: 200px;
-  width: 65%;
+  place-items: center;
+  overflow: hidden;
+  margin: auto;
 }
 
-.card-flip {
-  display: grid;
-  grid-template: 1fr / 1fr;
-  grid-template-areas: "frontAndBack";
-  transform-style: preserve-3d;
-  transition: all 0.7s ease;
+.box::before {
+  content: '';
+  position: absolute;
+  width: 500px;
+  height: 500px;
+  background-image: conic-gradient(transparent, transparent, transparent, #00ffad);
+  animation: rotate 4s linear infinite;
 }
 
-.card-flip div {
-  backface-visibility: hidden;
-  transform-style: preserve-3d;
+.box::after {
+  content: '';
+  position: absolute;
+  width: 500px;
+  height: 500px;
+  background-image: conic-gradient(transparent, transparent, transparent, #1842ad);
+  animation: rotate 4s linear infinite;
+  animation-delay: -2s;
 }
 
-.front {
-  grid-area: frontAndBack;
+.box span {
+  position: absolute;
+  inset: 5px;
+  background-radius: 16px;
+  background-color: #eae5e5;
+  z-index: 2;
 }
 
-.back {
-  grid-area: frontAndBack;
-  transform: rotateY(180deg);
+@keyframes rotate {
+  0% {
+    transform: rotate(0deg);
+  }
+  100% {
+    transform: rotate(360deg);
+  }
 }
 
-input[type='checkbox']:checked + .card-container .card-flip {
-  transform: rotateY(180deg);
+.definition {
+  text-position: auto;
+  margin: 8%;
+  color: #1c1d1e;
 }
 </style>
--- src/views/Testview.vue
+++ src/views/Testview.vue
@@ -1,165 +1,134 @@
 <template>
-  <head>
-    <title>Vocabularies</title>
-  </head>
-  <body>
-  <div class="container-fluid">
-    <div class="row row-cols 1 row-cols-md4 g-4">
-      <div class="col" v-for="vocabular in vocabularies" :key="vocabular.id">
-        <div class="box">
-    <span><h1>{{ vocabularies.indexOf(vocabular) + 1 }}/{{ vocabularies.length }}</h1>
-    <div class="word">
-      <h2>{{ vocabular.word }}</h2>
-    <div class="click">
-      <button class="btn btn-primary" @click="showDefinition(vocabular)">Show Definition</button>
-      </div>
-      <div class="definition" v-if="vocabular.showDefinition">
-        <h3>{{ vocabular.translation }}</h3>
-      </div>
-    </div>
-    </span>
+  <div class="container">
+    <input type="checkbox" id="card-1"/>
+    <label class="card-container" for="card-1">
+      <div class="card-flip">
+        <div class="front card">
+          <div class="card-block">
+            <h1 class="card-title text-center">{{ persons.firstName }}</h1>
+          </div>
+          <h1>Click on the card to turn around</h1>
+        </div>
+        <div class="back card">
+          <div class="card-block">
+            <h1 class="card-title">Back Card</h1>
+          </div>
+          <h1>Click on the card to turn around</h1>
         </div>
       </div>
-    </div>
+    </label>
   </div>
-  </body>
+  <button type="button" class="btn btn-primary left">
+    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-left"
+         viewBox="0 0 16 16">
+      <path fill-rule="evenodd"
+            d="M15 8a.5.5 0 0 0-.5-.5H2.707l3.147-3.146a.5.5 0 1 0-.708-.708l-4 4a.5.5 0 0 0 0 .708l4 4a.5.5 0 0 0 .708-.708L2.707 8.5H14.5A.5.5 0 0 0 15 8z"/>
+    </svg>
+  </button>
+  <button type="button" class="btn btn-primary middle">
+    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-clockwise"
+         viewBox="0 0 16 16">
+      <path fill-rule="evenodd" d="M8 3a5 5 0 1 0 4.546 2.914.5.5 0 0 1 .908-.417A6 6 0 1 1 8 2v1z"/>
+      <path d="M8 4.466V.534a.25.25 0 0 1 .41-.192l2.36 1.966c.12.1.12.284 0 .384L8.41 4.658A.25.25 0 0 1 8 4.466z"/>
+    </svg>
+  </button>
+  <button type="button" class="btn btn-primary right">
+    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-right"
+         viewBox="0 0 16 16">
+      <path fill-rule="evenodd"
+            d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8z"/>
+    </svg>
+  </button>
 </template>
 <script>
 export default {
-  name: 'Testview',
+  name: 'Learnview',
   data () {
     return {
-      vocabularies: [
-        {
-          id: 0,
-          word: 'Hello',
-          translation: 'Hallo'
-        },
-        {
-          id: 1,
-          word: 'learn',
-          translation: 'lernen'
-        },
-        {
-          id: 2,
-          word: 'study',
-          translation: 'studieren'
-        },
-        {
-          id: 3,
-          word: 'school',
-          translation: 'Schule'
-        },
-        {
-          id: 4,
-          word: 'university',
-          translation: 'Universität'
-        },
-        {
-          id: 5,
-          word: 'college',
-          translation: 'Hochschule'
-        },
-        {
-          id: 6,
-          word: 'teacher',
-          translation: 'Lehrer'
-        },
-        {
-          id: 7,
-          word: 'student',
-          translation: 'Student'
-        },
-        {
-          id: 8,
-          word: 'class',
-          translation: 'Klasse'
-        },
-        {
-          id: 9,
-          word: 'lesson',
-          translation: 'Unterricht'
-        },
-        {
-          id: 10,
-          word: 'exam',
-          translation: 'Prüfung'
-        },
-        {
-          id: 11,
-          word: 'test',
-          translation: 'Test'
-        }
-      ]
+      persons: []
     }
   },
-  methods: {
-    showDefinition (vocabular) {
-      vocabular.showDefinition = !vocabular.showDefinition
+  mounted () {
+    const requestOptions = {
+      method: 'GET',
+      redirect: 'follow'
     }
+
+    fetch('http://localhost:8080/api/persons', requestOptions)
+      .then(response => response.json())
+      .then(result => result.forEach(person => {
+        this.persons.push(person)
+      }))
+      .catch(error => console.log('error', error))
   }
 }
 </script>
 <style scoped>
-.word {
-  font-size: 1.5em;
-  font-weight: bold;
-  text-position: center;
-  margin: 25%;
+.btn-primary {
+  background-color: #1e90ff;
+  border-color: #1e90ff;
 }
 
-body {
-  background: #1c1d1e;
-  marign: 0;
+.card-title {
+  font-size: 2em;
+  font-weight: 800;
+  line-height: 3em;
+  margin: 0;
   padding: 0;
-  display: grid;
-  place-items: center;
-  min-height: 100vh;
+  text-align: center;
+  color: #141515;
 }
 
-.box {
+.card {
+  margin: 10px 10px;
+  border: 1px solid #ccc;
+  border-radius: 5px;
+  box-shadow: 0 1px 2px rgba(0, 0, 0, 0.2);
+  background-color: #f6f1f1;
+  transition: all 0.5s ease;
+  transform-style: preserve-3d;
   position: relative;
-  width: 300px;
-  height: 200px;
-  border-radius: 20px;
+  font-max-size: large;
+}
+
+.card-block {
+  padding: 10.5rem;
+}
+
+input[type='checkbox'] {
+  display: none;
+}
+
+.card-container {
   display: grid;
-  place-items: center;
-  overflow: hidden;
+  perspective: 700px;
+  margin-left: 200px;
+  width: 65%;
 }
 
-.box::before {
-  content: '';
-  position: absolute;
-  width: 500px;
-  height: 500px;
-  background-image: conic-gradient(transparent, transparent, transparent, #00ffad);
-  animation: rotate 4s linear infinite;
+.card-flip {
+  display: grid;
+  grid-template: 1fr / 1fr;
+  grid-template-areas: "frontAndBack";
+  transform-style: preserve-3d;
+  transition: all 0.7s ease;
 }
 
-.box::after {
-  content: '';
-  position: absolute;
-  width: 500px;
-  height: 500px;
-  background-image: conic-gradient(transparent, transparent, transparent, #1842ad);
-  animation: rotate 4s linear infinite;
-  animation-delay: -2s;
+.card-flip div {
+  backface-visibility: hidden;
+  transform-style: preserve-3d;
 }
 
-.box span {
-  position: absolute;
-  inset: 5px;
-  background-radius: 16px;
-  background-color: #eae5e5;
-  z-index: 2;
+.front {
+  grid-area: frontAndBack;
 }
 
-@keyframes rotate {
-  0% {
-    transform: rotate(0deg);
-  }
-  100% {
-    transform: rotate(360deg);
-  }
+.back {
+  grid-area: frontAndBack;
+  transform: rotateY(180deg);
+}
+
+input[type='checkbox']:checked + .card-container .card-flip {
+  transform: rotateY(180deg);
 }
 </style>
