741bbdf0969b731971c58517ab818d4773c79c5c
MohammedHammouri03 <97105790+MohammedHammouri03@users.noreply.github.com>
2022-12-05 00:02:45 +0100

    chart.js for data visualtion of the quiz

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -21,7 +21,11 @@ fetch('http://localhost:8080/api/score', requestOptions)
             'rgba(255, 206, 86, 0.2)',
             'rgba(75, 192, 192, 0.2)',
             'rgba(153, 102, 255, 0.2)',
-            'rgba(255, 159, 64, 0.2)'
+            'rgba(255, 159, 64, 0.2)',
+            'rgba(255, 99, 132, 0.2)',
+            'rgba(5, 1, 25, 0.1)',
+            'rgba(255, 206, 12, 0.2)',
+            'rgba(75, 192, 300, 0.2)'
           ],
           borderColor: [
             'rgba(255, 99, 132, 1)',
@@ -29,7 +33,11 @@ fetch('http://localhost:8080/api/score', requestOptions)
             'rgba(255, 206, 86, 1)',
             'rgba(75, 192, 192, 1)',
             'rgba(153, 102, 255, 1)',
-            'rgba(255, 159, 64, 1)'
+            'rgba(255, 159, 64, 1)',
+            'rgba(255, 99, 132, 1)',
+            'rgba(5, 1, 25, 1)',
+            'rgba(255, 206, 12, 1)',
+            'rgba(75, 192, 300, 1)'
           ],
           borderWidth: 1
         }]
